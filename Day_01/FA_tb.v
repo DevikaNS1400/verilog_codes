037c82a09f7f36473e9b7471bb3715f887bbc33c
N S Devika <nsdevika2000@gmail.com>
2025-07-23 09:04:16 +0530

    Update FA_tb.v

--- Day_01/FA_tb.v
+++ Day_01/FA_tb.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.07.2025 04:11:43
-// Design Name: 
-// Module Name: FA_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
