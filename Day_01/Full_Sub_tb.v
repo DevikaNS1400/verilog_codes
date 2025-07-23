1892b51afdb5c6cd1c93ab96a92e6a722ce7f26a
N S Devika <nsdevika2000@gmail.com>
2025-07-23 06:55:10 +0530

    Update Full_Sub_tb.v

--- Day_01/Full_Sub_tb.v
+++ Day_01/Full_Sub_tb.v
@@ -8,8 +8,9 @@ module FSub_tb;
 reg A,B,C;
 wire D,B_out;
 //Full_Subtractor u1(.D(D),.B_out(B_out),.A(A),.B(B),.C(C));//FSub_gtlvl
-FUll_Sub_Dataflow u2(.D(D),.B_out(B_out),.A(A),.B(B),.C(C));//FSub_dataflow
-//Full_Sub_Hsub u3(.D(D),.B_out(B_out),.A(A),.B(B),.C(C));//FSub_Hsub
+//FUll_Sub_Dataflow u2(.D(D),.B_out(B_out),.A(A),.B(B),.C(C));//FSub_dataflow
+FSub_HSub u3(.D(D),.B_out(B_out),.A(A),.B(B),.C(C));//FSub_Hsub
+
 initial
 begin
 
