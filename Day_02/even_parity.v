cf2b0c868ef659efdc4bab5dc8f47d3e6bc982d4
N S Devika <nsdevika2000@gmail.com>
2025-07-24 10:52:14 +0530

    Update even_parity.v

--- Day_02/even_parity.v
+++ Day_02/even_parity.v
@@ -6,7 +6,8 @@ module even_parity(
 output y,
 input [8:0]x
     );
-  wire y1,y2,y3,y4,y5,y6,y7;
+ // wire y1,y2,y3,y4,y5,y6,y7;
+    /*
   assign y1=x[0]^x[1];
   assign y2=x[2]^x[3];
   assign y3=x[4]^x[5];
@@ -15,4 +16,6 @@ input [8:0]x
   assign y6=y3^y4;
   assign y7=y5^y6;
   assign y=y7^x[8];
+  */
+assign y=(((x[0]^x[1])^(x[2]^x[3]))^((x[4]^x[5])^(x[6]^x[7])))^x[8];
 endmodule
