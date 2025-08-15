df3a6530ffd9e119d96e2c416afb4a7dd01ab2cf
N S Devika <nsdevika2000@gmail.com>
2025-08-15 21:52:34 +0530

    Add files via upload

---
+++ Day_04/mealy_ovlp_1010_tb.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module mealy_ovlp_tb();
+wire detect;
+reg din,clk,reset;
+mealy_ovlp u1(.detect(detect),.din(din),.clk(clk),.reset(reset));
+always #5 clk=~clk;
+initial begin
+clk=0;reset=1;#10;
+reset=0;din=1;#10;
+din=0;#10;
+din=1;#10;
+din=1;#10;
+din=0;#10;
+din=1;#10;
+din=0;#10;
+din=0;#10;
+$finish;
+end
+endmodule
