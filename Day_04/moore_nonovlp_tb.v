75b04dba80047cb4d7e235a5f4e9b0d4623115fd
N S Devika <nsdevika2000@gmail.com>
2025-08-15 22:35:15 +0530

    Add files via upload

---
+++ Day_04/moore_nonovlp_tb.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+module moore_nonovlp_tb();
+wire detect;
+reg din,clk,reset;
+moore_nonovlp u1(.detect(detect),.din(din),.clk(clk),.reset(reset));
+always #5 clk=~clk;
+initial begin
+clk=0;reset=1;#10;
+reset=0;din=1;#10;
+din=0;#10;
+din=1;#10;
+din=1;#10;
+din=0;#10;
+din=1;#10;
+din=0;#10;
+din=1;#10;
+din=0;#10;
+din=0;#10;
+$finish;
+end
+endmodule
